63c410c07bd3189181d3ae5a24bfa7d4858017f8
Hoshiki Tsunoda <liquidohr@gmail.com>
2018-01-12 01:38:57 -0800

    modified scss so expanded image appears in the center. #7

--- app/js/main.js
+++ app/js/main.js
@@ -7,7 +7,21 @@ for(let i = 0; i < $image.length; i++) {
   $image[i].addEventListener('mouseover', function(event) {
     event.target.classList.add('image-hover')
   })
+  $image[i].addEventListener('click', function() {
+    $detail.classList.remove('hidden')
+  })
   $image[i].addEventListener('mouseleave', function(event) {
     event.target.classList.remove('image-hover')
   })
 }
+
+for(let i = 0; i < $image.length; i++) {
+  $image[i].addEventListener('click', function(event) {
+    const a = event.target.getAttribute('src')
+    $bigImage.setAttribute('src', a)
+  })
+}
+
+$overlay.addEventListener('click', function(event) {
+  $detail.classList.add('hidden')
+})
