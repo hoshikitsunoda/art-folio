1e5d1e3053e80d75bc7ebe05a522f27c056018d5
Hoshiki Tsunoda <liquidohr@gmail.com>
2018-01-11 17:39:49 -0800

    add click eventlistener on each image to add overlay class. #5

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,12 @@
 const $image = document.querySelectorAll('img')
+const $detail = document.querySelector('#detail')
 
 for(let i = 0; i < $image.length; i++) {
   $image[i].addEventListener('mouseover', function(event) {
     event.target.classList.add('image-hover')
+  })
+  $image[i].addEventListener('click', function() {
+    $detail.classList.add('overlay')
   })
   $image[i].addEventListener('mouseleave', function(event) {
     event.target.classList.remove('image-hover')
