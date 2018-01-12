69e55c55936e33627096ef905f989f3beb7425eb
Hoshiki Tsunoda <liquidohr@gmail.com>
2018-01-12 03:16:02 -0800

    cleaned js files. #10

--- app/js/main.js
+++ app/js/main.js
@@ -1,26 +1,22 @@
-const $image = document.querySelectorAll('.art')
 const $detail = document.querySelector('#detail')
 const $bigImage = document.querySelector('#big-image')
 const $overlay = document.querySelector('.overlay')
 
-for(let i = 0; i < $image.length; i++) {
-  $image[i].addEventListener('mouseover', function(event) {
+$image.forEach(image => {
+  image.addEventListener('mouseover', function(event) {
     event.target.classList.add('image-hover')
   })
-  $image[i].addEventListener('click', function() {
+  image.addEventListener('click', function() {
     $detail.classList.remove('hidden')
   })
-  $image[i].addEventListener('mouseleave', function(event) {
+  image.addEventListener('mouseleave', function(event) {
     event.target.classList.remove('image-hover')
   })
-}
-
-for(let i = 0; i < $image.length; i++) {
-  $image[i].addEventListener('click', function(event) {
+  image.addEventListener('click', function(event) {
     const a = event.target.getAttribute('src')
     $bigImage.setAttribute('src', a)
   })
-}
+})
 
 $overlay.addEventListener('click', function(event) {
   $detail.classList.add('hidden')
