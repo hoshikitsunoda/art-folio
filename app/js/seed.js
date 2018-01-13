4d7fe95d67df3ff57cd215d22b0f431c104e0b8f
Hoshiki Tsunoda <liquidohr@gmail.com>
2018-01-13 12:55:35 -0800

    added id to each object. #16

--- app/js/seed.js
+++ app/js/seed.js
@@ -1,93 +1,116 @@
 const artData = [
   {
+    id: 'plant',
     title: 'Plant',
     year: '2017'
   },
   {
+    id: 'butterfly',
     title: 'Butterfly',
     year: '2016'
   },
   {
+    id: 'dance',
     title: 'Zen Dance',
     year: '2017'
   },
   {
+    id: 'faces',
     title: 'Faces',
     year: '2017'
   },
   {
+    id: 'strange',
     title: 'Dr. Very Strange',
     year: '2017'
   },
   {
+    id: 'yannar',
     title: 'Yan Nar',
     year: '2017'
   },
   {
+    id: 'korea',
     title: 'Korean Insiration',
     year: '2016'
   },
   {
+    id: 'saga',
     title: 'Romancing Saga',
     year: '2017'
   },
   {
+    id: 'oni-fish',
     title: 'Oni-Fish',
     year: '2016'
   },
   {
+    id: 'moon',
     title: 'Moon',
     year: '2016'
   },
   {
+    id: 'towerofsun',
     title: 'Tower of Sun',
     year: '2017'
   },
   {
+    id: 'woman',
     title: 'Woman in the Mirror',
     year: '2017'
   },
   {
+    id: 'selfportrait',
     title: 'Selfportrait',
     year: '2017'
   },
   {
+    id: 'sunegg',
     title: 'Sun Egg',
     year: '2016'
   },
   {
+    id: 'treeoflife',
     title: 'Tree of Life',
     year: '2015'
   },
   {
+    id: 'dieturn',
     title: 'Die Turn',
     year: '2015'
   },
   {
+    id: 'void',
     title: 'Void',
     year: '2016'
   },
   {
+    id: 'silva',
     title: 'Silva',
     year: '2015'
   },
   {
+    id: 'skullcandy',
     title: 'Skull Candy',
     year: '2016'
   },
   {
+    id: 'octo',
     title: 'Octo',
     year: '2016'
   },
   {
+    id: 'liquidbrain',
     title: 'Liquid Brain',
     year: '2017'
   },
   {
+    id: 'colorfro',
     title: 'Color-Fro',
     year: '2017'
   },
   {
+    id: 'fresh',
     title: 'Fresh!',
     year: '2016'
   }
