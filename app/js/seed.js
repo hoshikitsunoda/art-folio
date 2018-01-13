e9da89efdfbac3977cf9150d5d639e80ab77ce22
Hoshiki Tsunoda <liquidohr@gmail.com>
2018-01-13 11:59:18 -0800

    add art info as object in seed.js #16

---
+++ app/js/seed.js
@@ -0,0 +1,94 @@
+const artData = [
+  {
+    title: 'Plant',
+    year: '2017'
+  },
+  {
+    title: 'Butterfly',
+    year: '2016'
+  },
+  {
+    title: 'Zen Dance',
+    year: '2017'
+  },
+  {
+    title: 'Faces',
+    year: '2017'
+  },
+  {
+    title: 'Dr. Very Strange',
+    year: '2017'
+  },
+  {
+    title: 'Yan Nar',
+    year: '2017'
+  },
+  {
+    title: 'Korean Insiration',
+    year: '2016'
+  },
+  {
+    title: 'Romancing Saga',
+    year: '2017'
+  },
+  {
+    title: 'Oni-Fish',
+    year: '2016'
+  },
+  {
+    title: 'Moon',
+    year: '2016'
+  },
+  {
+    title: 'Tower of Sun',
+    year: '2017'
+  },
+  {
+    title: 'Woman in the Mirror',
+    year: '2017'
+  },
+  {
+    title: 'Selfportrait',
+    year: '2017'
+  },
+  {
+    title: 'Sun Egg',
+    year: '2016'
+  },
+  {
+    title: 'Tree of Life',
+    year: '2015'
+  },
+  {
+    title: 'Die Turn',
+    year: '2015'
+  },
+  {
+    title: 'Void',
+    year: '2016'
+  },
+  {
+    title: 'Silva',
+    year: '2015'
+  },
+  {
+    title: 'Skull Candy',
+    year: '2016'
+  },
+  {
+    title: 'Octo',
+    year: '2016'
+  },
+  {
+    title: 'Liquid Brain',
+    year: '2017'
+  },
+  {
+    title: 'Color-Fro',
+    year: '2017'
+  },
+  {
+    title: 'Fresh!',
+    year: '2016'
+  }
+]
