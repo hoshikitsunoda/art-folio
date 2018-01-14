83015c76851612d3497ddb791c0c0d42658c73bc
Hoshiki Tsunoda <liquidohr@gmail.com>
2018-01-13 22:14:37 -0800

    added painting data to seed.js #19

--- app/js/seed.js
+++ app/js/seed.js
@@ -138,3 +138,54 @@ const artData = [
     url: 'https://goo.gl/xEh7L2'
   }
 ]
+
+const paintingData = [
+  {
+    id: 'moon',
+    title: 'Moon',
+    year: '2016',
+    url: 'https://goo.gl/5dn4Hu'
+  },
+  {
+    id: 'towerofsun',
+    title: 'Tower of Sun',
+    year: '2017',
+    url: 'https://goo.gl/54LPwT'
+  },
+  {
+    id: 'woman',
+    title: 'Woman in the Mirror',
+    year: '2017',
+    url: 'https://goo.gl/Bq2jTp'
+  },
+  {
+    id: 'selfportrait',
+    title: 'Selfportrait',
+    year: '2017',
+    url: 'https://goo.gl/NHHvqb'
+  },
+  {
+    id: 'sunegg',
+    title: 'Sun Egg',
+    year: '2016',
+    url: 'https://goo.gl/73drtp'
+  },
+  {
+    id: 'treeoflife',
+    title: 'Tree of Life',
+    year: '2015',
+    url: 'https://goo.gl/LTKQjP'
+  },
+  {
+    id: 'dieturn',
+    title: 'Die Turn',
+    year: '2015',
+    url: 'https://goo.gl/Xg2L9U'
+  },
+  {
+    id: 'void',
+    title: 'Void',
+    year: '2016',
+    url: 'https://goo.gl/xRZfhe'
+  }
+]
