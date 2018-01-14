dadee33bb5abe79d8f5dd03b982b42858f3a7d79
Hoshiki Tsunoda <liquidohr@gmail.com>
2018-01-14 14:09:19 -0800

    add another variable of objects for drawing in seed.js #21

--- app/js/seed.js
+++ app/js/seed.js
@@ -189,3 +189,60 @@ const paintingData = [
     url: 'https://goo.gl/xRZfhe'
   }
 ]
+
+const drawingData =[
+  {
+    id: 'plant',
+    title: 'Plant',
+    year: '2017',
+    url: 'https://goo.gl/ZcwgcA'
+  },
+  {
+    id: 'butterfly',
+    title: 'Butterfly',
+    year: '2016',
+    url: 'https://goo.gl/fjxrfM'
+  },
+  {
+    id: 'dance',
+    title: 'Zen Dance',
+    year: '2017',
+    url: 'https://goo.gl/eFeFxx'
+  },
+  {
+    id: 'faces',
+    title: 'Faces',
+    year: '2017',
+    url: 'https://goo.gl/6L6KXH'
+  },
+  {
+    id: 'strange',
+    title: 'Dr. Very Strange',
+    year: '2017',
+    url: 'https://goo.gl/WU4fbC'
+  },
+  {
+    id: 'yannar',
+    title: 'Yan Nar',
+    year: '2017',
+    url: 'https://goo.gl/LKKuW7'
+  },
+  {
+    id: 'korea',
+    title: 'Korean Inspiration',
+    year: '2016',
+    url: 'https://goo.gl/5vYUjC'
+  },
+  {
+    id: 'saga',
+    title: 'Romancing Saga',
+    year: '2017',
+    url: 'https://goo.gl/yGjuuT'
+  },
+  {
+    id: 'oni-fish',
+    title: 'Oni-Fish',
+    year: '2016',
+    url: 'https://goo.gl/sED7Bc'
+  }
+]
