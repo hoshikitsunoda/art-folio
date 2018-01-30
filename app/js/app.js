0f99605b59d17528f5e3cc4bc71da837a1de588d
Hoshiki Tsunoda <liquidohr@gmail.com>
2018-01-30 12:35:54 -0800

    removed commented out codes. #31

--- app/js/app.js
+++ app/js/app.js
@@ -1,16 +1,11 @@
 $(function() {
-    // Get the form.
   var form = $('#message-form');
 
-    // Get the messages div.
   var formMessages = $('#form-messages');
 
-    // TODO: The rest of the code will go here...
   $(form).submit(function(event) {
-      // Stop the browser from submitting the form.
     event.preventDefault();
 
-      // TODO
     var formData = $(form).serialize();
 
     $.ajax({
@@ -19,24 +14,19 @@ $(function() {
       data: formData
     })
     .done(function(response) {
-        // Make sure that the formMessages div has the 'success' class.
       $(formMessages).removeClass('error');
       $(formMessages).addClass('success');
 
-        // Set the message text.
       $(formMessages).text(response);
 
-        // Clear the form.
       $('#name').val('');
       $('#email').val('');
       $('#message').val('');
     })
     .fail(function(data) {
-        // Make sure that the formMessages div has the 'error' class.
         $(formMessages).removeClass('success');
         $(formMessages).addClass('error');
 
-        // Set the message text.
         if (data.responseText !== '') {
             $(formMessages).text(data.responseText);
         } else {
